21a3cc942f2e9c613c25d2dbefbe24fd903176c9
Elżbieta Janus-Kaleta <60756867+ElaJK01@users.noreply.github.com>
2021-11-12 12:20:53 +0100

    template engin ejs install and first templates

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/login', (req, res) => {
+  res.render('login')
 })
 
 app.listen(port, () => {
